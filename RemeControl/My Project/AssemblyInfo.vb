ae87b4ff0e3053aa014de86696f30968ffccf8d3
PedroM85 <pedromaneiro@gmail.com>
2023-03-05 11:55:08 -0300

    minor

--- RemeControl/My Project/AssemblyInfo.vb
+++ RemeControl/My Project/AssemblyInfo.vb
@@ -1,4 +1,4 @@
-﻿Imports System
+Imports System
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 
@@ -31,5 +31,7 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.1.1.1")>
-<Assembly: AssemblyFileVersion("1.1.1.1")>
+<Assembly: AssemblyVersion("1.1.23064.2")>
+<Assembly: AssemblyFileVersion("1.1.23064.2")>
+
+<assembly: AssemblyInformationalVersion("0.0.23064.1")>
